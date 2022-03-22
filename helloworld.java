52476072532c8c119c6333a77599ec82641e23cf
Akash <iapplehacker@yahoo.com>
2022-03-22 19:37:24 +0100

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello, World. My name is akash suresh and my student id is 50195. );
+    }
+
+}
